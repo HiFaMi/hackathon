c3f9b4d3b7188fd1f113fd3b13c615e61dfc050b
mingyu <rlaalsrbgk@gmail.com>
2018-07-20 00:27:44 +0900

    재료 및 foodstuff model 작성

--- app/members/models.py
+++ app/members/models.py
@@ -13,3 +13,50 @@ class User(AbstractUser):
     email = models.EmailField(blank=True)
     authority = models.CharField(max_length=1, choices=AUTHORITY)
     # order = models.ManyToManyField()
+
+
+class BaseIce(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+
+
+class Size(models.Model):
+    FOOD_SIZE = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('B', 'Big'),
+    )
+
+    food_size = models.CharField(max_length=1, choices=FOOD_SIZE)
+
+
+class FoodStuff(models.Model):
+    base_ice = models.ForeignKey(
+        BaseIce,
+        on_delete=models.CASCADE
+    )
+
+    source = models.ForeignKey(
+        Source,
+        on_delete=models.CASCADE
+    )
+
+    topping = models.ForeignKey(
+        Topping,
+        on_delete=models.CASCADE
+    )
+
+    size = models.ForeignKey(
+        Size,
+        on_delete=models.CASCADE
+    )
